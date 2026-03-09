749bf11041c9eaaf1a2fabf9e8954774fe8f64ae
shinseongcheol <cher3725@gmail.com>
2026-03-09 17:02:40 +0900

    feat 'camera'
    - 불필요한 라우트 정리

--- backend/app/api/v1/endpoints/cameras.py
+++ backend/app/api/v1/endpoints/cameras.py
@@ -17,24 +17,6 @@ def run_camera_worker(cam_id, rtsp_url):
     worker.run()
 
 router = APIRouter()
-@router.get("/")
-async def read(request: Request):
-    camera_service = request.app.state.camera_service
-    frame: np.ndarray = camera_service.read()
-
-    _, buffer = cv2.imencode('.jpg', frame)
-
-    return Response(content=buffer.tobytes(), media_type="image/jpeg")
-
-@router.get("/stream")
-async def read_stream(request: Request, db:Session = Depends(async_get_db)):
-    camera_service = request.app.state.camera_service
-    insightface_service = request.app.state.insightface_service
-
-    return StreamingResponse(
-        camera_service.stream(),
-        media_type="multipart/x-mixed-replace; boundary=frame"
-    )
 
 @router.post("/{camera_id}/start")
 async def start_camera(request: Request, camera_id:int):
